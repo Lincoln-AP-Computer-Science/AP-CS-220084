1b276820d878f48a8ad8917fe3e34ecc44d4878b
Chai Nunes <chaiteanunes@gmail.com>
2016-08-24 08:52:57 -0700

    Update DrawBoxes.java

--- classwork/8-24-16/DrawBoxes.java
+++ classwork/8-24-16/DrawBoxes.java
@@ -1,14 +1,26 @@
 public class DrawBoxes {
     public static void main (String[] args) {
-        System.out.println("+------+");
-        System.out.println("|      |");
-        System.out.println("|      |");
-        System.out.println("+------+");
+        for (int i = 0; i < 2; i++) {
+            box();
+        }
+    }
+    
+    void box () {
+        for (int i = 0; i < 4; i++) {
+            if (i > 0 && i < 3) {
+               edged();
+            } else {
+               cornered();
+            }
+        }
         System.out.println();
+    }
+    
+    void cornered () {
         System.out.println("+------+");
+    }
+    
+    void edged () {
         System.out.println("|      |");
-        System.out.println("|      |");
-        System.out.println("+------+");
-        System.out.println();
     }
 }
