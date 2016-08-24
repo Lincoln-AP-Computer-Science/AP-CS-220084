e7fe93cfaba5beb10e5b66961e1a1287dab5220c
Chai Nunes <chaiteanunes@gmail.com>
2016-08-24 09:08:12 -0700

    Update DrawBoxes.java

--- classwork/8-24-16/DrawBoxes.java
+++ classwork/8-24-16/DrawBoxes.java
@@ -1,7 +1,8 @@
 public class DrawBoxes {
     public static void main (String[] args) {
-        for (int i = 0; i < 2; i++) {
-            box();
+        for (int i = 0; i < 3; i++) {
+            DrawBoxes boxes = new DrawBoxes();
+            boxes.box();
         }
     }
     
