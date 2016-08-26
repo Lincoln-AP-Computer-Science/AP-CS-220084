d52c2f92c2f2a5a36e7b2249742a1be56e3ae15a
Chai Nunes <chaiteanunes@gmail.com>
2016-08-26 08:59:39 -0700

    Update PP_1_3.java

--- PP/PP_1_3.java
+++ PP/PP_1_3.java
@@ -1,3 +1,11 @@
 public class PP_1_3 {
+    String[] objs = {"fly", "spider", "bird", "cat", "dog", "cow", "horse"};
 
+    public static void main(String[] args) {
+        PP_1_3 p = new PP_1_3();
+    }
+    
+    void oldLady(String obj) {
+        System.out.println("There was an old lady who swallowed a " + obj);
+    }
 }
