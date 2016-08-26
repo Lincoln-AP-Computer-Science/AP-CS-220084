a9e0ecc2e1f89b58b3133f71924a893509aa4466
Chai Nunes <chaiteanunes@gmail.com>
2016-08-26 08:37:48 -0700

    Update PP_1_5.java

--- PP/PP_1_5.java
+++ PP/PP_1_5.java
@@ -1,7 +1,6 @@
 public class PP_1_5 {
-    String jackBuilt = "hosue that Jack built.\n";
-    String[] objs = {"malt", "rat", "cat", "dog", "cow with the crumpled horn", "maiden all forlorn"};
-    String[] verbs = {"lay", "ate", "killed", "worried", "tossed", "milked"};
+    String[] objs = {"the house that Jack built.\n", "the malt", "the rat,", "the cat,", "the dog,", "the cow with the crumpled horn,", "the maiden all forlorn,"};
+    String[] verbs = {"lay in", "ate", "killed", "worried", "tossed", "milked"};
 
     public static void main(String[] args) {
         PP_1_5 p = new PP_1_5();
@@ -9,23 +8,21 @@ public class PP_1_5 {
     }
     
     void startln(String s) {
-        System.out.println("This is the " + s);
+        System.out.println("This is " + s);
     }
     
-    void that(String end) {
-        System.out.println("That " + end);
+    void that(String verb, String obj) {
+        System.out.println("That " + verb + " " + obj);
     }
     
     void song() {
         for(int i = 0; i < objs.length; i++) {
             for (int j = i; j >= 0; j--) {
-                if(j == i) {
-                    if(i > 0) {
-                        startln(objs[j] + ",");
-                    } else {
-                        startln(objs[j]);
-                    }
-                } 
+                if (j == i) {
+                    startln(objs[j]);
+                } else {
+                    that(verbs[j], objs[j]);
+                }
             }
         }
     }
