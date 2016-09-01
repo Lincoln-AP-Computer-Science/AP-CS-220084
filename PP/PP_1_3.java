3afec7420d2680956aaa5d8ed415abf16295ddc5
Chai Nunes <chaiteanunes@gmail.com>
2016-09-01 08:44:55 -0700

    Update PP_1_3.java

--- PP/PP_1_3.java
+++ PP/PP_1_3.java
@@ -6,6 +6,9 @@ public class PP_1_3 {
         p.fly();
         p.spider();
         p.bird();
+        p.cat();
+        p.dog();
+        p.horse();
     }
     
     void oldLady(String obj) {
@@ -59,22 +62,47 @@ public class PP_1_3 {
         System.out.println("How absurd! She swallowed a bird!");
         reason("bird");
         middle();
-        System.out.println("She'll leave us high and dry.");
+        System.out.println("She'll leave us high and dry.\n");
     }
     
     void cat() {
-    
+        oldLady("cat");
+        System.out.println("Imagine that! She swallowed a cat.");
+        reason("cat");
+        middle();
+        System.out.println("I hope it's a lie.\n");
     }
     
     void dog() {
-    
+        oldLady("dog");
+        System.out.println("She went whole hog to swallow the dog.");
+        reason("dog");
+        middle();
+        System.out.println("There's a tear in my eye.\n");
+        
     }
     
     void cow() {
-    
+        oldLady("cow");
+        System.out.println("I don't know how she swallowed the cow.");
+        reason("cow");
+        middle();
+        System.out.println("I'd rather have ham on rye.");
+        System.out.println("And she had a frog on the sly.");
+        System.out.println("She did it in one try.\n");
     }
     
     void horse() {
-    
+        oldLady("horse");
+        System.out.println("She dies, of course.");
+        System.out.println("It was the last course.");
+        System.out.println("I'm filled with remorse.");
+        System.out.println("What's left to say...");
+        System.out.println("Even the artist is crying.");
+        System.out.println("We'll miss her dearly.");
+        System.out.println("It is such a loss.");
+        System.out.println("She had no time to floss.");
+        System.out.println("She missed out on the sauce.\n");
+        System.out.println("Moral: Never swallow a horse.");
     }
 }
