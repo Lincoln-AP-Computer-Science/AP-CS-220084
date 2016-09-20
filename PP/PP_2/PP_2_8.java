41bb5863eda13d2bae58f8892af1c39e67b4c56b
Chai Nunes <chaiteanunes@gmail.com>
2016-09-19 17:00:08 -0700

    Create PP_2_8.java

---
+++ PP/PP_2/PP_2_8.java
@@ -0,0 +1,74 @@
+public class PP_2_8 {
+
+    public static void main(String[] args) {
+        final int size = 10;
+
+        for (int i = 0; i < size + 1; i++) {
+            System.out.print(" ");
+        }
+        System.out.print("+");
+        for (int i = 0; i < size * 3; i++) {
+            System.out.print("-");
+        }
+        System.out.println("+");
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size - i; j++) {
+                System.out.print(" ");
+            }
+            System.out.print("/");
+            for (int j = 0; j < size - (i + 1); j++) {
+                System.out.print("   ");
+            }
+            for (int j = 0; j < i + 1; j++) {
+                if (j == 0) {
+                    System.out.print("___");
+                } else {
+                    System.out.print("/__");
+                }
+            }
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print("/");
+            }
+            System.out.println();
+        }
+
+        System.out.print("+");
+        for (int i = 0; i < size * 3; i++) {
+            System.out.print("-");
+        }
+        System.out.print("+");
+        for (int i = 0; i < size; i++) {
+            System.out.print("/");
+        }
+        System.out.println();
+
+        for (int i = 0; i < size / 2; i++) {
+            System.out.print("|");
+            if (size % 2 == 0 && size * 3 > 22) {
+                for (int j = 0; j < (size * 3 - 22) / 2; j++) {
+                    System.out.print(" ");
+                }
+                System.out.print("Building Java Programs");
+                for (int j = 0; j < (size * 3 - 22) / 2; j++) {
+                    System.out.print(" ");
+                }
+            } else {
+                for (int j = 0; j < size * 3; j++) {
+                    System.out.print(" ");
+                }
+            }
+            System.out.print("|");
+            for (int j = 0; j < size / 2 - i; j++) {
+                System.out.print("//");
+            }
+            System.out.println();
+        }
+
+        System.out.print("+");
+        for (int i = 0; i < size * 3; i++) {
+            System.out.print("-");
+        }
+        System.out.print("+");
+    }
+}
