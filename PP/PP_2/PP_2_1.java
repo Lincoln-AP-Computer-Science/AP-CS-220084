081907da87c238635042850b8fdec35b1895bf5b
Chai Nunes <chaiteanunes@gmail.com>
2016-09-19 17:12:01 -0700

    Update PP_2_1.java

--- PP/PP_2/PP_2_1.java
+++ PP/PP_2/PP_2_1.java
@@ -1,5 +1,7 @@
 // Write a progam that produces [a specific pattern].
+
 public class PP_2_1 {
+    
     public static void main(String[] args) {
         for(int i = 7; i > 0; i--) {
             for(int j = 0; j < i - 1; j++) {
