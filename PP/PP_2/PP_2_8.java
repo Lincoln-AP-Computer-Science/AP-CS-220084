b15879a88c959b7c63fc7e774f16422b510f7245
Chai Nunes <chaiteanunes@gmail.com>
2016-09-21 08:11:55 -0700

    Fix syntax
    `final int size` to `final int SIZE`

--- PP/PP_2/PP_2_8.java
+++ PP/PP_2/PP_2_8.java
@@ -4,23 +4,23 @@
 public class PP_2_8 {
     
     public static void main(String[] args) {
-        final int size = 10;
+        final int SIZE = 10;
 
-        for (int i = 0; i < size + 1; i++) {
+        for (int i = 0; i < SIZE + 1; i++) {
             System.out.print(" ");
         }
         System.out.print("+");
-        for (int i = 0; i < size * 3; i++) {
+        for (int i = 0; i < SIZE * 3; i++) {
             System.out.print("-");
         }
         System.out.println("+");
 
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size - i; j++) {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE - i; j++) {
                 System.out.print(" ");
             }
             System.out.print("/");
-            for (int j = 0; j < size - (i + 1); j++) {
+            for (int j = 0; j < SIZE - (i + 1); j++) {
                 System.out.print("   ");
             }
             for (int j = 0; j < i + 1; j++) {
@@ -37,39 +37,39 @@ public class PP_2_8 {
         }
 
         System.out.print("+");
-        for (int i = 0; i < size * 3; i++) {
+        for (int i = 0; i < SIZE * 3; i++) {
             System.out.print("-");
         }
         System.out.print("+");
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < SIZE; i++) {
             System.out.print("/");
         }
         System.out.println();
 
-        for (int i = 0; i < size / 2; i++) {
+        for (int i = 0; i < SIZE / 2; i++) {
             System.out.print("|");
-            if (size % 2 == 0 && size * 3 > 22) {
-                for (int j = 0; j < (size * 3 - 22) / 2; j++) {
+            if (SIZE * 3 > 22) {
+                for (int j = 0; j < (SIZE * 3 - 22) / 2; j++) {
                     System.out.print(" ");
                 }
                 System.out.print("Building Java Programs");
-                for (int j = 0; j < (size * 3 - 22) / 2; j++) {
+                for (int j = 0; j < (SIZE * 3 - 22) / 2; j++) {
                     System.out.print(" ");
                 }
             } else {
-                for (int j = 0; j < size * 3; j++) {
+                for (int j = 0; j < SIZE * 3; j++) {
                     System.out.print(" ");
                 }
             }
             System.out.print("|");
-            for (int j = 0; j < size / 2 - i; j++) {
+            for (int j = 0; j < SIZE / 2 - i; j++) {
                 System.out.print("//");
             }
             System.out.println();
         }
 
         System.out.print("+");
-        for (int i = 0; i < size * 3; i++) {
+        for (int i = 0; i < SIZE * 3; i++) {
             System.out.print("-");
         }
         System.out.print("+");
