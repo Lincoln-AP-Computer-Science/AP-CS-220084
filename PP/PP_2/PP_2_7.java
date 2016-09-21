d4f12e7c8d9e325e342464c01aebd526dabe38f2
Chai Nunes <chaiteanunes@gmail.com>
2016-09-21 09:31:48 -0700

    Update PP_2_7.java

--- PP/PP_2/PP_2_7.java
+++ PP/PP_2/PP_2_7.java
@@ -1,1 +1,50 @@
-
+public class PP_2_7 {
+    final int SIZE = 4;
+    
+    public static void main(String[] args) {
+        PP_2_7 p = new PP_2_7();
+        
+        p.tower();
+        
+        for (int i = 0; i < p.SIZE; i++) {
+            for (int j = 0; j < (p.SIZE - (i + 1)) * 3; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i + 1; j++) {
+                if (j == 0) {
+                    System.out.print("__/");
+                } else {
+                    System.out.print(":::");
+                }
+            }
+            System.out.print("||");
+            for (int j = 0; j < i + 1; j++) {
+                if (j == i) {
+                    System.out.print("\\__");
+                } else {
+                    System.out.print(":::");
+                }
+            }
+            System.out.println();
+        }
+        
+        p.divider();
+    }
+    
+    public void tower() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE * 3; j++) {
+                System.out.print(" ");
+            }
+            System.out.println("||");
+        }
+    }
+    
+    public void divider() {
+        System.out.print("|");
+        for (int i = 0; i < SIZE * 6; i++) {
+            System.out.print("\"");
+        }
+        System.out.println("|");
+    }
+}
