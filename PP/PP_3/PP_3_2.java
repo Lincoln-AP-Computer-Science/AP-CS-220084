5f4f4f003feec3c2e2f86ac7f59254c14339da69
Chai Nunes <chaiteanunes@gmail.com>
2016-09-22 08:59:55 -0700

    Update PP_3_2.java

--- PP/PP_3/PP_3_2.java
+++ PP/PP_3/PP_3_2.java
@@ -1,1 +1,24 @@
-
+public class PP_3_2 {
+    
+    public static void main(String[] args) {
+        PP_3_2 p = new PP_3_2();
+        
+        p.interest(1000, 100, 6.25);
+    }
+    
+    public void interest(int inital, int deposit, double interest) {
+        double balance = inital;
+        System.out.println("+-----------------------------------------------------------+");
+        System.out.println("|\tYear\t|\tInterest\t|\tNew Deposit\t|\tNew Balance\t|");
+        for (int i = 0; i <= 25; i++) {
+            System.out.print("|\t" + i + "\t\t");
+            System.out.print("|\t" + interest  + "\t\t");
+            if (i == 0) {
+                System.out.print("|\t" + 0 + "\t\t\t");
+            } else {
+                System.out.print("|\t" + deposit + "\t\t\t");
+            }
+            System.out.println("|\t" + balance + "\t\t|");
+        }
+    }
+}
