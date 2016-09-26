467975589abc3816d7c9ed77919cb7bb00349d44
Chai Nunes <chaiteanunes@gmail.com>
2016-09-26 08:03:15 -0700

    Update PP_2_1.java

--- PP/PP_2/PP_2_1.java
+++ PP/PP_2/PP_2_1.java
@@ -3,26 +3,40 @@
 public class PP_2_1 {
     
     public static void main(String[] args) {
+        PP_2_1 p = new PP_2_1();
+        
         for(int i = 7; i > 0; i--) {
-            for(int j = 0; j < i - 1; j++) {
-                System.out.print("*");
-            }
-            for(int j = 0; j < 8 - i; j++) {
-                System.out.print(" ");
-            }
-            for(int j = 0; j < i - 1; j++) {
-                System.out.print("//");
-            }
-            for(int j = 0; j < 7 - i; j++) {
-                System.out.print("\\\\");
-            }
-            for(int j = 0; j < 8 - i; j++) {
-                System.out.print(" ");
-            }
-            for(int j = 0; j < i - 1; j++) {
-                System.out.print("*");
-            }
+            p.leftCurtain(i);
+            p.centerPattern(i);
+            p.rightCurtain(i);
             System.out.println();
+        }
+    }
+    
+    public void leftCurtain(int i) {
+        for (int j = 0; j < i - 1; j++) {
+                System.out.print("*");
+        }
+        for (int j = 0; j < 8 - i; j++) {
+            System.out.print(" ");
+        }
+    }
+    
+    public void centerPattern(int i) {
+        for(int j = 0; j < i - 1; j++) {
+            System.out.print("//");
+        }
+        for(int j = 0; j < 7 - i; j++) {
+            System.out.print("\\\\");
+        }
+    }
+    
+    public void rightCurtain(int i) {
+        for(int j = 0; j < 8 - i; j++) {
+            System.out.print(" ");
+        }
+        for(int j = 0; j < i - 1; j++) {
+            System.out.print("*");
         }
     }
 }
