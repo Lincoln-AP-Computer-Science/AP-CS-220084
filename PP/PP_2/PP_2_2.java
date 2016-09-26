fa6adf46fbbdf672eb31fc979f566e076ad137b3
Chai Nunes <chaiteanunes@gmail.com>
2016-09-26 08:10:25 -0700

    make object oriented

--- PP/PP_2/PP_2_2.java
+++ PP/PP_2/PP_2_2.java
@@ -3,58 +3,46 @@
 public class PP_2_2 {
 
     public static void main(String[] args) {
-        System.out.print("+");
-        for (int i = 0; i < 6; i++) {
-            System.out.print("-");
-        }
-        System.out.println("+");
+        PP_2_2 p = new PP_2_2();
+        
+        p.divider();
 
         for (int i = 0; i < 2; i++) {
-            for (int j = 0; j < 3; j++) {
-                System.out.print("|");
-                for (int k = 0; k < 2 - j; k++) {
-                    System.out.print(" ");
-                }
-                System.out.print("^");
-                for (int k = 0; k < j; k++) {
-                    System.out.print("  ");
-                }
-                System.out.print("^");
-                for (int k = 0; k < 2 - j; k++) {
-                    System.out.print(" ");
-                }
-                System.out.println("|");
-            }
+            p.arrow("^");
         }
 
-        System.out.print("+");
-        for (int i = 0; i < 6; i++) {
-            System.out.print("-");
-        }
-        System.out.println("+");
+        p.divider();
 
         for (int i = 0; i < 2; i++) {
-            for (int j = 0; j < 3; j++) {
-                System.out.print("|");
-                for (int k = 0; k < 2 - j; k++) {
-                    System.out.print(" ");
-                }
-                System.out.print("v");
-                for (int k = 0; k < j; k++) {
-                    System.out.print("  ");
-                }
-                System.out.print("v");
-                for (int k = 0; k < 2 - j; k++) {
-                    System.out.print(" ");
-                }
-                System.out.println("|");
-            }
+            p.arrow("v");
         }
 
+        p.divider();
+    }
+    
+    public void divider() {
         System.out.print("+");
         for (int i = 0; i < 6; i++) {
             System.out.print("-");
         }
         System.out.println("+");
+    }
+    
+    public void arrow(String character) {
+        for (int j = 0; j < 3; j++) {
+            System.out.print("|");
+            for (int k = 0; k < 2 - j; k++) {
+                System.out.print(" ");
+            }
+            System.out.print(character);
+            for (int k = 0; k < j; k++) {
+                System.out.print("  ");
+            }
+            System.out.print(character);
+            for (int k = 0; k < 2 - j; k++) {
+                System.out.print(" ");
+            }
+            System.out.println("|");
+        }
     }
 }
