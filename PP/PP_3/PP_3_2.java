67f24aac19015745f2ed835461e8b870d2d87299
Chai Nunes <chaiteanunes@gmail.com>
2016-09-28 08:30:26 -0700

    Update PP_3_2.java

--- PP/PP_3/PP_3_2.java
+++ PP/PP_3/PP_3_2.java
@@ -1,3 +1,5 @@
+// [Compound Interest]
+
 public class PP_3_2 {
 
     public static void main(String[] args) {
@@ -8,28 +10,40 @@ public class PP_3_2 {
 
     public void interest(int inital, int deposit, double interest) {
         double balance = inital;
+        String strbalance = Double.toString(balance);
         System.out.print("+");
-        for(int i = 0; i < 19; i++) {
-            System.out.print("----");
+        for(int i = 0; i < 59; i++) {
+            System.out.print("-");
         }
         System.out.println("+");
         System.out.println("|\tYear\t|\tInterest\t|\tNew Deposit\t|\tNew Balance\t|");
         for (int i = 0; i <= 25; i++) {
-            System.out.print("|\t" + i + "\t");
+            for (int j = 0; j < strbalance.length(); j++) {
+                if(strbalance.charAt(j) == '.') {
+                    if (j < strbalance.length() - 2) {
+                        strbalance = strbalance.substring(0, j + 3);
+                    } else if (j < strbalance.length() - 1) {
+                        strbalance = strbalance.substring(0, j + 2) + "0";
+                    }
+                }
+            }
+        
+            System.out.print("|\t" + i + "\t\t");
             System.out.print("|\t" + interest + "%\t\t");
             if (i == 0) {
-                System.out.print("|\t" + 0 + "\t\t");
+                System.out.print("|\t" + 0 + "\t\t\t");
             } else {
-                System.out.print("|\t" + deposit + "\t\t");
+                System.out.print("|\t" + deposit + "\t\t\t");
             }
-            System.out.println("|\t" + balance + "\t\t|");
+            System.out.println("|\t" + strbalance + "\t\t|");
 
-            balance += deposit;
-            balance += inital * (1 + interest / 100);
+            balance = deposit * (i + 1);
+            balance += inital * Math.pow((1 + interest / 100), i + 1);
+            strbalance = Double.toString(balance);
         }
-         System.out.print("+");
-        for(int i = 0; i < 19; i++) {
-            System.out.print("----");
+        System.out.print("+");
+        for(int i = 0; i < 59; i++) {
+            System.out.print("-");
         }
         System.out.println("+");
     }
