2b4fda624c4f9b718617f486d4b767cde606b29e
Chai Nunes <chaiteanunes@gmail.com>
2016-09-28 11:52:46 -0700

    Begin PP 3.6

--- PP/PP_3/PP_3_6.java
+++ PP/PP_3/PP_3_6.java
@@ -1,1 +1,18 @@
+// Write a program that produces calendars as output
 
+public class PP_3_6 {
+
+    public static void main(String[] args) {
+        PP_3_6 p = new PP_3_6();
+        
+        p.calendar(6, 31);
+    }
+
+    public void calendar(int firstSunday, int days) {
+        if (firstSunday > 1) {
+            int[][] cal = new int[5][7];
+        }
+        System.out.println("  Sun    Mon    Tue    Wed    Thu    Fri    Sat");
+        System.out.println("+------+------+------+------+------+------+------+");
+    }
+}
