e698e64a247ce7c3a521a9d31c6850c6ed148139
Chai Nunes <chaiteanunes@gmail.com>
2016-09-28 11:31:35 -0700

    Update PP_3_4.java

--- PP/PP_3/PP_3_4.java
+++ PP/PP_3/PP_3_4.java
@@ -5,16 +5,18 @@ public class PP_3_4 {
     public static void main(String[] args) {
         PP_3_4 p = new PP_3_4();
         
-        p.getAngles(8, 6, 7);
+        System.out.println(p.getAngles(8, 6, 7));
     }
     
-    public void getAngles(double a, double b, double c) {
+    public String getAngles(double a, double b, double c) {
         double cosA = (b * b + c * c - a * a) / (2 * b * c);
         double cosB = (a * a + c * c - b * b) / (2 * a * c);
         double cosC = (a * a + b * b - c * c) / (2 * a * b);
+        double aVal = Math.toDegrees(Math.acos(cosA));
+        double bVal = Math.toDegrees(Math.acos(cosB));
+        double cVal = Math.toDegrees(Math.acos(cosC));
+        String result = aVal + ", " + bVal + ", " + cVal;
         
-        return {180 * Math.acos(cosA) / Math.PI,
-                180 * Math.acos(cosB) / Math.PI,
-                180 * Math.acos(cosC) / Math.PI};
+        return result;
     }
 }
