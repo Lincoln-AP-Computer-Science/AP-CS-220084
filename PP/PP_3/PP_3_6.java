f266fecc8d16d54ee235a7eb9418db79029bf586
Chai Nunes <chaiteanunes@gmail.com>
2016-09-29 08:48:09 -0700

    Update PP_3_6.java

--- PP/PP_3/PP_3_6.java
+++ PP/PP_3/PP_3_6.java
@@ -9,10 +9,53 @@ public class PP_3_6 {
     }
 
     public void calendar(int firstSunday, int days) {
-        if (firstSunday > 1) {
-            int[][] cal = new int[5][7];
-        }
+        int columns;
         System.out.println("  Sun    Mon    Tue    Wed    Thu    Fri    Sat");
         System.out.println("+------+------+------+------+------+------+------+");
+        
+        if (days == 28 && firstSunday == 1) {
+            columns = 4;
+        } else {
+            if (firstSunday == 1) {
+                columns = 5;
+            } else {
+                if (5 * 7 - (8 - firstSunday) >= 31) {
+                    columns = 5;
+                } else {
+                    columns = 6;
+                }
+            }
+        }
+        
+        int d = 1;
+        for (int i = 0; i < columns; i++) {
+            for (int j = 0; j < 7; j++) {
+                if (i == 0 && j < 8 - firstSunday && firstSunday > 1) {
+                    if (j == 0) {
+                        System.out.print("|      |");
+                    } else {
+                        System.out.print("      |");
+                    }
+                } else if (d <= days) {
+                    String prefix;
+                    if (j == 0) {
+                        prefix = "|  ";
+                    } else {
+                        prefix = "  ";
+                    }
+                    
+                    if (d < 10) {
+                        System.out.print(prefix + d++ + "   |");
+                    } else {
+                        System.out.print(prefix + d++ + "  |");
+                    }
+                } else {
+                    System.out.print("      |");
+                }
+            }
+            System.out.println();
+        }
+        
+        System.out.println("+------+------+------+------+------+------+------+");    
     }
 }
