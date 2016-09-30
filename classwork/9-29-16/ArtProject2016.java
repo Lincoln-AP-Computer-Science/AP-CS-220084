7558d6eea3c47f0b0c4378d61a9f08ca6f17b1b2
Chai Nunes <chaiteanunes@gmail.com>
2016-09-30 08:22:27 -0700

    Update ArtProject2016.java

--- classwork/9-29-16/ArtProject2016.java
+++ classwork/9-29-16/ArtProject2016.java
@@ -3,11 +3,27 @@ public class ArtProject2016 {
     public static void main(String[] args) {
         ArtProject2016 art = new ArtProject2016();
         
-        art.writeChars('=', 20);
-        for (int i = 0; i < 10; i++) {
-            art.writeChars('>', i);
-            art.writeChars(' ', 20 - 2 * i);
-            art.writeChars('>', i);
+        for (int i = -5; i <= 5; i++) {
+            if(i % 2 == 0) {
+                art.writeChars(' ', Math.abs(i * 2));
+            } else {
+                art.writeChars(':', Math.abs(i * 2));
+            }
+            for (int j = 0; j <= 5; j++) {
+                if (i >= -5 + j && i <= 5 - j) {
+                    art.writeChars('#', 1);
+                    art.writeChars(' ', 2);
+                }
+            }
+            if(i % 2 == 0) {
+                art.writeChars(' ', Math.abs(i));
+            } else {
+                art.writeChars(':', Math.abs(i));
+            }
+            art.writeChars(':', Math.abs(i) + 1);
+            art.writeChars('#', 5 - Math.abs(i));
+            art.writeChars(' ', 5 - Math.abs(i));
+            art.writeChars(':', Math.abs(i) + 1);
             System.out.println();
         }
     }
