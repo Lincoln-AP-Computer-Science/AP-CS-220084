8ab0f4319950616f98c4ee6316e385d896e2b34c
Chai Nunes <chaiteanunes@gmail.com>
2016-10-13 08:56:23 -0700

    WIP

--- classwork/10-12-16/ExerciseFour.java
+++ classwork/10-12-16/ExerciseFour.java
@@ -1,15 +1,56 @@
+import java.util.*;
+
 public class ExerciseFour {
     
     public static void main(String[] args) {
         ExerciseFour ef = new ExerciseFour();
+        
+        int[][] points = {
+                {0, 0}, 
+                {3, 4}, 
+                {3, 0}
+            };
+        
+        System.out.print(ef.calculate(points));
     }
     
     public double distance(int x1, int y1, int x2, int y2) {
         return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
     }
     
-    public void calculate(int[][] allPoints) {
-        int[] points = new int[allPoints.length];
-        System.out.print(points.length);
+    public double calculate(int[][] allPoints) {
+        int[][] points = allPoints;
+        double[][] dist = new double[points.length - 1][points.length];
+        int x1, y1, x2, y2;
+        double longestDistance = dist[0][0];
+//         for(int i = 0; i < points.length; i++) {
+//             System.out.println(Arrays.toString(points[i]));
+//         }
+        double[] distances = new double[points.length];
+        for (int i = 0; i < points.length; i++) {
+            x1 = points[i][0];
+            y1 = points[i][1];
+            
+            for (int j = 0; j < points.length; j++) {
+                if (j != i) {
+                    x2 = points[j][0];
+                    y2 = points[j][1];
+                    System.out.println("i: " + i + "; j: " + j);
+                    for (int k = 0; k < dist.length; k++) {
+                        dist[k][j] = distance(x1, y1, x2, y2);
+                    }
+                }
+            }
+        }
+        
+        for (int k = 0; k < dist.length; k++) {
+            for (int l = 0; l < dist[0].length; l++) {
+                if (dist[k][l] > longestDistance) {
+                    longestDistance = dist[k][l];
+                }
+            }
+        }
+        
+        return longestDistance;
     }
 }
