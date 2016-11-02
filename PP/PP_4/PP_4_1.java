290e05c82fde25a03bc5d2b5f631e30010c7b76e
Chai Nunes <chaiteanunes@gmail.com>
2016-11-02 08:41:54 -0700

    Update PP_4_1.java

--- PP/PP_4/PP_4_1.java
+++ PP/PP_4/PP_4_1.java
@@ -4,43 +4,62 @@ public class PP_4_1 {
     
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        PP_4_1 pp = new PP_4_1();
         
+        int input = 0;
+        
+        
+        while (input < 1 || input > 1000) {
+            System.out.println("Input a number between 1 and 1000 to convert to Roman Numerals:");
+            input = sc.nextInt();
+            if (input < 1) {
+                System.out.println("Try Again! Input number should be greater than or equal to 1.");
+            } else if (input > 1000) {
+                System.out.println("Try Again! Input number should be less than or equal to 1000.");
+            }
+        }
+        
+        System.out.println(pp.convertToRomanNumerals(input));
+    }
+    
+    public String convertToRomanNumerals(int n) {
         int[] numbers = {1, 5, 10, 50, 100, 500, 1000};
         char[] letters = {'I', 'V', 'X', 'L', 'C', 'D', 'M'};
+        int num = n, index = 0;
+        char[] outputChars = new char[12];
+
         
-        int input, index = 0;
-        char[] outputChars = new char[112]; // Largest number in Roman Numerals is 888 (DCCCLXXXVIII)
-        String outputString;
-        
-        System.out.println("Input a number to convert to Roman Numerals:");
-        input = sc.nextInt();
-        
-        
-        System.out.println(input);
-        while (input != 0) {
+        while (num != 0) {
             for (int i = numbers.length; i > 0; i--) {
-                if (input >= numbers[i - 1]) {
-                    input -= numbers[i - 1];
-                    outputChars[index] = letters[i - 1];
-                    index++;
-                    break;
-                } else if (input >= numbers[i - 1] - 1) {
-                    input -= numbers[i - 1] - 1;
-                    outputChars[index] = letters[0];
-                    index++;
+                boolean met = false;
+                if (num >= numbers[i - 1]) {
+                    met = true;
+                } else {
+                    for (int j = 100, k = 4; j > 0; j /= 10, k -= 2) {
+                        if (num >= numbers[i - 1] - j && num > j) {
+                            num += j;
+                            outputChars[index] = letters[k];
+                            index++;
+                            met = true;
+                            break;
+                        }
+                    }
+                    
+                }
+                if (met) {
+                    num -= numbers[i - 1];
                     outputChars[index] = letters[i - 1];
                     index++;
                     break;
                 }
             }
         }
-        System.out.println(input);
+        
         char[] endChars = new char[index];
         for (int i = 0; i < endChars.length; i++) {
             endChars[i] = outputChars[i];
         }
         
-        outputString = new String(endChars);
-        System.out.println(outputString);
+        return String.valueOf(endChars);
     }
 }
