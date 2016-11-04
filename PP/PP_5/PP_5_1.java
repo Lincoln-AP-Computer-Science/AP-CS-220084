c69f3e8a4928d3d6ccb963eb2d6376c8acff304f
Chai Nunes <chaiteanunes@gmail.com>
2016-11-04 08:35:22 -0700

    Update PP_5_1.java

--- PP/PP_5/PP_5_1.java
+++ PP/PP_5/PP_5_1.java
@@ -3,6 +3,16 @@ import java.util.*;
 public class PP_5_1 {
 
     public static void main(String[] args) {
+        PP_5_1 pp = new PP_5_1();
+        while (true) {
+            if (!pp.run()) {
+                System.out.println("Terminated");
+                break;
+            }
+        }
+    }
+    
+    public boolean run() {
         Scanner sc = new Scanner(System.in);
         String[] input;
         String output = "";
@@ -10,27 +20,30 @@ public class PP_5_1 {
         
         System.out.println("Input a sentence with no punctuation:");
         input = sc.nextLine().split("\\s+");
-        
-        for (int i = 0; i < input.length; i++) {
-            String firstSound = "";
-            for (int j = 0; j < input[i].length(); j++) {
-                if (firstSound == "") {
-                    for (int k = 0; k < vowels.length; k++) {
-                        if (j > 0 && input[i].charAt(j) == vowels[k]) {
-                            firstSound = input[i].substring(0, j);
-                            input[i] = input[i].replace(firstSound, "");
-                            break;
+            
+        if (input[0].length() > 0) {
+            for (int i = 0; i < input.length; i++) {
+                String firstSound = "";
+                for (int j = 0; j < input[i].length(); j++) {
+                    if (firstSound == "") {
+                        for (int k = 0; k < vowels.length; k++) {
+                            if (input[i].charAt(j) == vowels[k]) {
+                                firstSound = input[i].substring(0, j);
+                                break;
+                            }
                         }
+                    } else {
+                        break;
                     }
-                } else {
-                    break;
                 }
+                input[i] = input[i].concat("-" + firstSound + "ay");
+                output = output.concat(input[i] + " ");
             }
-            input[i] = input[i].concat("-" + firstSound + "ay");
-            output = output.concat(input[i] + " ");
+            
+            output = output.trim();
+            System.out.println(output);
+            return true;
         }
-        
-        output = output.trim();
-        System.out.println(output);
+        return false;
     }
 }
