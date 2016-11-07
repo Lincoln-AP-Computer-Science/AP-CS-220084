c38529aa4725103caf5f8d266d5511fbe167d137
Chai Nunes <chaiteanunes@gmail.com>
2016-11-07 08:26:53 -0800

    Update PP_5_2.java

--- PP/PP_5/PP_5_2.java
+++ PP/PP_5/PP_5_2.java
@@ -7,33 +7,42 @@ public class PP_5_2 {
         Scanner sc = new Scanner(System.in);
         int fails_left = 6, letterCount, correct = 0;
         char[] letters_left = {
-            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
-        };
+            'e', 't', 'a', 'o', 'i', 'n', 's', 'r', 'h', 'l', 'd', 'c', 'u',
+            'm', 'f', 'p', 'g', 'w', 'y', 'b', 'v', 'k', 'x', 'j', 'q', 'z'
+        }; // Most to least common
         
         System.out.println("How many letters are in the word?");
         letterCount = sc.nextInt();
         
+        
         while (fails_left > 0 && correct < letterCount) {
-            if(!pp.run()) {
+            char input = ' ';
+            int guess;
+            if (fails_left >= 3) {
+                guess = (int) (Math.random() * 12);
+            } else {
+                guess = (int) (Math.random() * letters_left.length);
+            }
+            while (input != 'y' && input != 'n') {
+                System.out.println("Is there a " + letters_left[guess] + "? (Y/N)");
+                input = sc.next().charAt(0);
+            }
+            
+            char[] temp = new char[letters_left.length - 1];
+            for (int i = 0, j = 0; i < letters_left.length; i++) {
+                if (i != guess) {
+                    temp[j++] = letters_left[i];
+                }
+            }
+            letters_left = temp;
+           
+            if (input == 'y' || input == 'Y') {
+                System.out.println("How many?");
+                correct += sc.nextInt();
+            } else {
                 fails_left--;
+                System.out.println(fails_left + " tries left");
             }
         }
-        
-    }
-    
-    public boolean run(char[] letters) {
-        Scanner sc = new Scanner(System.in);
-        String input;
-        int rand = Math.random() * letters_left.length();
-        while (input != "y" && input != "n") {
-            System.out.println("Is there a " + letters[rand] + "? (Y/N)");
-            input = sc.nextString().toLowerCase();
-        }
-        
-        if (input = "y") {
-            return true;
-        }
-        return false;
     }
 }
