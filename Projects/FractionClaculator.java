2917878abd53c116ace0df23223719053e2a2aa1
Chai Nunes <chaiteanunes@gmail.com>
2016-11-17 08:59:59 -0800

    Update FractionClaculator.java

--- Projects/FractionClaculator.java
+++ Projects/FractionClaculator.java
@@ -1,8 +1,25 @@
 import java.util.*;
 
 public class FractionCalculator {
-    
+
     public static void main(String[] args) {
+        FractionCalculator fc = new FractionCalculator();
+        Scanner sc = new Scanner(System.in);
+        String input;
+        
+        while (true) {
+            System.out.print("Input an equation: ");
+            input = sc.nextLine();
+            
+            if (input.equalsIgnoreCase("quit")) break;
+            
+            fc.calculate(input);
+        }
+        
+        System.out.println("Thank you for using my fractional calculator.");
+    }
     
+    public int calculate(String input) {
+        return 0;
     }
 }
