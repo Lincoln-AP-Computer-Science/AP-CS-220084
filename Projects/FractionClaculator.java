fa84719550ea735075c28b42f8715750959263a1
Chai Nunes <chaiteanunes@gmail.com>
2016-11-21 08:40:58 -0800

    Update FractionClaculator.java

--- Projects/FractionClaculator.java
+++ Projects/FractionClaculator.java
@@ -1,6 +1,7 @@
 import java.util.*;
 
 public class FractionCalculator {
+    public final char[] operators = {'*', '/', '+', '-'};
 
     public static void main(String[] args) {
         FractionCalculator fc = new FractionCalculator();
@@ -13,13 +14,46 @@ public class FractionCalculator {
             
             if (input.equalsIgnoreCase("quit")) break;
             
-            fc.calculate(input);
+            double result = fc.calculate(input);
+            System.out.println(input + " = " + result);
         }
         
         System.out.println("Thank you for using my fractional calculator.");
     }
     
-    public int calculate(String input) {
-        return 0;
+    public double calculate(String input) {
+        input = input.replace(" ", "");
+        char operator = ' ';
+        double num1, num2;
+        int opIndex = input.indexOf(operator);
+        
+        for (int i = 0; i < input.length(); i++) {
+            for (char op : operators) {
+                if (input.charAt(i) == op) {
+                    operator = op;
+                    opIndex = input.indexOf(operator);
+                    break;
+                }
+            }
+        }
+        
+        if (opIndex <= 0) {
+            try {
+                return Integer.parseInt(input);
+            } catch (Exception e) {
+                return 0;
+            }
+        }
+        
+        num1 = Integer.parseInt(input.substring(0, opIndex));
+        num2 = Integer.parseInt(input.substring(opIndex + 1));
+        
+        switch (operator) {
+            case '*': return num1 * num2;
+            case '/': return num1 / num2;
+            case '+': return num1 + num2;
+            case '-': return num1 - num2;
+            default: return 0;
+        }
     }
 }
