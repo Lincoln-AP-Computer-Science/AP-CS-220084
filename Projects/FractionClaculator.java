0f4a8ef1e35c7b4299b96ef5f1eaa702caa16924
Chai Nunes <chaiteanunes@gmail.com>
2016-12-02 09:00:10 -0800

    Update FractionClaculator.java

--- Projects/FractionClaculator.java
+++ Projects/FractionClaculator.java
@@ -7,34 +7,94 @@ public class FractionCalculator {
         Scanner sc = new Scanner(System.in);
         String equation, answer;
         
-        System.out.println("Input an equation:");
-        equation = sc.nextLine();
+        while (true) {
+            System.out.println("Input an expression or type QUIT to quit:");
+            equation = sc.nextLine();
+            
+            if (equation.equals("QUIT")) {
+                break;
+            }
+            
+            answer = fc.calculate(equation);
+            if (answer != "FAIL") {
+                System.out.println("The ouput is " + answer);
+            } else {
+                System.out.println("Invalid expression!");
+            }
+        }
+    }
+    
+    public int[] improperFraction(int[] components) {
+        int[] output = new int[2];
+        boolean isNegative = false;
+        for (int i : components) {
+            if(i < 0) isNegative = true;
+        }
         
-        answer = fc.calculate(equation);
-        System.out.print("The ouput is " + answer);
+        output[0] = Math.abs(components[0] * components[2]) + components[1];
+        output[1] = Math.abs(components[2]);
+        
+        if (isNegative) output[0] = -output[0];
+        return output;
     }
     
     public String multiply(String operand1, String operand2) {
-        return new String();
+        String parsed1, parsed2;
+        int[] components1, components2;
+        
+        parsed1 = parseFraction(operand1);
+        parsed2 = parseFraction(operand2);
+        
+        components1 = improperFraction(getComponents(parsed1));
+        components2 = improperFraction(getComponents(parsed2));
+        
+        return parseFraction((components1[0] * components2[0]) + "/" + (components1[1] * components2[1]));
     }
     
     public String divide(String operand1, String operand2) {
-        return new String();
+        String parsed1, parsed2;
+        int[] components2;
+        
+        components2 = improperFraction(getComponents(operand2));
+        
+        parsed1 = parseFraction(operand1);
+        parsed2 = parseFraction(components2[1] + "/" + components2[0]);
+        
+        return multiply(parsed1, parsed2);
     }
     
     public String add(String operand1, String operand2) {
+        String parsed1, parsed2;
+        int[] components1, components2;
+        int numerator, denominator;
         
-        return new String();
+        components1 = improperFraction(getComponents(operand1));
+        components2 = improperFraction(getComponents(operand2));
+        
+        numerator = components1[0] * components2[1] + components2[0] * components1[1];
+        denominator = components1[1] * components2[1];
+        
+        return parseFraction(numerator + "/" + denominator);
     }
     
     public String subtract(String operand1, String operand2) {
-        return new String();
+        String parsed1, parsed2;
+        int[] components1, components2;
+        int numerator, denominator;
+        
+        components1 = improperFraction(getComponents(operand1));
+        components2 = improperFraction(getComponents(operand2));
+        
+        numerator = components1[0] * components2[1] - components2[0] * components1[1];
+        denominator = components1[1] * components2[1];
+        
+        return parseFraction(numerator + "/" + denominator);
     }
 
     public String calculate(String expression){
         int firstBlank = expression.indexOf(' ');
+        String answer = "";
         String operand1, operand2;
-        String parsed1, parsed2;
         char operator;
         
         if (firstBlank >= 0) {
@@ -42,35 +102,37 @@ public class FractionCalculator {
             operand2 = expression.substring(firstBlank + 3);
             operator = expression.charAt(firstBlank + 1);
             
-            parsed2 = parseOperand(operand2);
-            
             switch (operator) {
-                case '*':
-                case '/':
-                case '+':
-                case '-':
+                case '*': answer = multiply(operand1, operand2); break;
+                case '/': answer = divide(operand1, operand2); break;
+                case '+': answer = add(operand1, operand2); break;
+                case '-': answer = subtract(operand1, operand2); break;
             }
             
         } else {
-            operand1 = expression;
-            operand2 = "0";
-            operator = ' ';
-            
-            parsed2 = "";
+            return "FAIL";
         }
         
-        parsed1 = parseOperand(operand1);
-        
-        return parsed1 + operator + parsed2;
+        return answer;
     }
     
-    public String parseOperand(String operand) {
+    public String parseFraction(String operand) {
+        String output;
         int[] components = getComponents(operand);
         int wholeNumber, numerator, denominator;
+        boolean isNegative = false;
         
         wholeNumber = components[0];
         numerator = components[1];
         denominator = components[2];
+        
+        if (wholeNumber < 0 || numerator < 0 || denominator < 0) {
+            isNegative = true;
+        }
+        
+        wholeNumber = Math.abs(wholeNumber);
+        numerator = Math.abs(numerator);
+        denominator = Math.abs(denominator);
         
         wholeNumber += (int) (numerator / denominator);
         numerator %= denominator;
@@ -99,10 +161,24 @@ public class FractionCalculator {
         numerator /= gcd;
         denominator /= gcd;
         
-        if (numerator != 0) {
-            return wholeNumber + "_" + numerator + "/" + denominator;
+        if (isNegative) {
+            output = "-";
         } else {
-            return Integer.toString(wholeNumber);
+            output = "";
+        }
+        
+        if (denominator != 0) {
+            if (wholeNumber != 0) {
+                output += Integer.toString(wholeNumber) + "_";
+            }
+            
+            if (numerator != 0) {
+                output += numerator + "/" + denominator;
+            }
+            
+            return output;
+        } else {
+            return "undefined";
         }
     }
     
