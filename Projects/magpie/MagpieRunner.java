6efd4f742a1e9dfc19ffb8e60084e43862800156
Chai Nunes <chaiteanunes@gmail.com>
2017-01-11 08:50:18 -0800

    Create MagpieRunner.java

---
+++ Projects/magpie/MagpieRunner.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class MagpieRunner {
+	public static void main(String[] args) {
+		Magpie maggie = new Magpie();
+      Scanner sc = new Scanner (System.in);
+      String name, input;
+		
+      System.out.println(maggie.getName());
+      name = sc.nextLine();
+		System.out.println (maggie.getGreeting(name));
+      input = sc.nextLine();
+		
+		while (!input.equals("Bye")) {
+			System.out.println (maggie.getResponse(input));
+			input = sc.nextLine();
+		}
+	}
+
+}
