27bff87278221ca6f091cd6ac3938e0c7ac9eb36
Chai Nunes <chaiteanunes@gmail.com>
2017-01-18 08:07:07 -0800

    Update MagpieRunner.java

--- Projects/magpie/MagpieRunner.java
+++ Projects/magpie/MagpieRunner.java
@@ -1,20 +1,20 @@
 import java.util.*;
 
 public class MagpieRunner {
-	public static void main(String[] args) {
-		Magpie maggie = new Magpie();
+    public static void main(String[] args) {
+        Magpie maggie = new Magpie();
         Scanner sc = new Scanner (System.in);
         String name, input;
-		
+
         System.out.println(maggie.getName());
         name = sc.nextLine();
         System.out.println (maggie.getGreeting(name));
         input = sc.nextLine();
-		
-		while (!input.equals("Bye")) {
-	        System.out.println (maggie.getResponse(input));
-			input = sc.nextLine();
-		}
+
+        while (!input.equals("Bye")) {
+            System.out.println (maggie.getResponse(input));
+            input = sc.nextLine();
+        }
         System.out.println("Goodbye!");
-	}
+    }
 }
