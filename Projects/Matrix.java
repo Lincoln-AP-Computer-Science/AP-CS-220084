5b40680383b8ae99ee907fac86b6589a45f44a21
Chai Nunes <chaiteanunes@gmail.com>
2017-02-06 08:19:21 -0800

    Update Matrix.java

--- Projects/Matrix.java
+++ Projects/Matrix.java
@@ -3,11 +3,12 @@ import java.util.*;
 public class Matrix {
     
     private double[][] matrix;
+    private int size;
     
     public Matrix(int size, double... numbers) {
+        this.size = size;
         this.matrix = new double[size][size];
-        int index = 0;
-        for (int row = 0; row < size; row++) {
+        for (int row = 0, index = 0; row < size; row++) {
             for (int col = 0; col < size; col++) {
                 this.matrix[row][col] = numbers[index];
                 index++;
@@ -15,14 +16,19 @@ public class Matrix {
         }
     }
     
+    public Matrix(int size) {
+        this.size = size;
+        this.matrix = new double[size][size];
+    }
+    
     @Override
     public String toString() {
         String output = "";
         for (int row = 0; row < this.matrix.length; row++) {
             for (int col = 0; col < this.matrix.length; col++) {
-                if (col == 0) output += "|";
-                output += this.matrix[row][col];
-                if (col == matrix.length - 1) output += "|";
+                if (col == 0) output += "| ";
+                output += this.matrix[row][col] + " ";
+                if (col == matrix.length - 1) output += "|\n";
             }
         }
         return output;
