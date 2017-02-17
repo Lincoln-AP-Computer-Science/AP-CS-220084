53cb133df5102bf2fe09170bc4d5998b8e74c1ca
Chai Nunes <chaiteanunes@gmail.com>
2017-02-17 08:41:36 -0800

    Create Dessert.java

---
+++ classwork/semester2/2-17-2017/Dessert.java
@@ -0,0 +1,43 @@
+public class Dessert {
+    // Private Variables
+    private double price, weight;
+    private int calories;
+    
+    // Public Constructors
+    public Dessert() {
+        this.price = 3.00;
+        this.weight = 3.0;
+        this.calories = 500;
+    }
+    
+    public Dessert(double price, double weight, int calories) {
+        this.price = price;
+        this.weight = weight;
+        this.calories = calories;
+    }
+    
+    // Public Methods
+    public double getPrice() {
+        return price;
+    }
+    
+    public double getWeight() {
+        return weight;
+    }
+    
+    public int getCalories() {
+        return calories;
+    }
+    
+    public void setPrice(double price) {
+        if (price >= 0) this.price = price;
+    }
+    
+    public void setWeight(double weight) {
+        if (weight > 0) this.weight = weight;
+    }
+    
+    public void setCalories(int calories) {
+        if (calories > 0) this.calories = calories;
+    }
+}
