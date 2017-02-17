1d1a6d105a28159ca9006cfa32d802aa8000f2ed
Chai Nunes <chaiteanunes@gmail.com>
2017-02-17 09:10:12 -0800

    Create DessertTest.java

---
+++ classwork/semester2/2-17-2017/DessertTest.java
@@ -0,0 +1,9 @@
+public class DessertTest {
+    
+    public static void main(String[] args) {
+        Dessert cake = new Dessert(5.00, 5.0, 800);
+        Sundae bananaSplit = new Sundae(2.50, 4.0, 700, "Chocolate Syrup", "Banana", "Cherry");
+        System.out.println(cake);
+        System.out.println(bananaSplit);
+    }
+}
